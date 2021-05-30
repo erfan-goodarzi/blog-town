f4c46d00cb472dc55116917b57a261862577a8a8
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-05-30 23:18:10 +0430

    Finish Navbar style, Add Register and login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
  
 import './App.css';
 import Layout from './Components/Layout/Layout'
-import {BrowserRouter} from 'react-router-dom'
+
 function App() {
   return (
-    <BrowserRouter>
+  
+      
     <div className="App">
       <Layout/>
     </div>
-    </BrowserRouter>
+
   );
 }
 
