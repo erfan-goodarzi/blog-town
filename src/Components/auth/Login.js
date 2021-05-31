6cadd351085a88bedf6638bdd1aab1b5eb08fba7
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-05-31 22:59:27 +0430

    Update Login.js

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -50,53 +50,54 @@ export default class Login extends Component {
     return (
       <div>
         <div className="container">
-          
-     
-        <form onSubmit={this.handleSubmit}>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
+          <div className="row mt-5">
+            <div className="col-lg-6 mx-auto">
+            <form onSubmit={this.handleSubmit}>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  required
+                />
+                <div id="emailHelp" className="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" for="exampleCheck1">
+                  Check me out
+                </label>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
             </div>
           </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              value={this.state.password}
-            onChange={this.handleChange}
-            required
-            />
-          </div>
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-
         </div>
         {/* <form onSubmit={this.handleSubmit}>
           <input
