b45576f2efcd62b609ef36d1c266712397acde23
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-06-10 00:00:03 +0430

    Change Login style

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./Login.css";
 
 export default class Login extends Component {
   constructor(props) {
@@ -52,53 +53,60 @@ export default class Login extends Component {
         <div className="container">
           <div className="row mt-5">
             <div className="col-lg-4 mx-auto">
-            <form onSubmit={this.handleSubmit}>
-              <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
-                <input
-                   type="email"
-                   name="email"
-                   className="form-control"
-                   placeholder="Email"
-                   value={this.state.email}
-                   onChange={this.handleChange}
-                   required
-                />
+              <div className="card">
+                <h4 className="text-primary fw-bold  ms-3 mt-3">Login</h4>
+                <div className="card-body">
+                  
+                <form onSubmit={this.handleSubmit}>
+                <div className="mb-3">
+                  <label for="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" for="exampleCheck1">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L80,144C160,128,320,96,480,122.7C640,149,800,235,960,266.7C1120,299,1280,277,1360,266.7L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
               </div>
-              <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  className="form-control"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                  required
-                />
-              </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" for="exampleCheck1">
-                  Check me out
-                </label>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
+              
             </div>
           </div>
         </div>
-      
       </div>
     );
   }
