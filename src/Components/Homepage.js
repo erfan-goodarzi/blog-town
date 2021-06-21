c930cd0827470ffe3a893139ba149dec4cfa0816
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-06-21 23:42:03 +0430

    GET api and show response

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,33 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Layout/Header";
-import Post from "./Posts/Post";
+ import Post from "./Posts/Post";
 import TopPost from "./Posts/TopPost";
-import PostImg from "../assets/image/boeing_787_9_516453.jpg";
-import PostImg2 from "../assets/image/woman-wearing-virtual-reality-simulator_23-2148910255.jpg";
-import PostImg3 from "../assets/image/3d-illustration-astronaut-pose-against-isolated-black-background-with-clipping-path_46363-369.jpg";
 
-const Homepage = () => {
-  return (
-    <div>
+import axios from "axios";
+
+class Homepage extends Component {
+  state={
+    posts: [],
+   
+}
+
+
+
+componentDidMount(){
+    axios.get('http://api.mediastack.com/v1/news?access_key=d073b39f8f21673d0431971f9a3c490c')
+    .then(res => {
+      console.log(res)
+      const posts = res.data.data;
+      console.log(posts)
+        const updatePosts = posts.map(item=>{
+            return {
+                ...item,
+                author: 'Erfan'
+            }
+        })
+        this.setState({posts: updatePosts})
+    }).catch(error =>{
+        this.setState({error :true})
+    })
+    
+}
+
+postClick = (id) =>{
+    this.setState({selectedPostID : id})
+}
+
+render() {
+    let posts = <h4 style={{textAlign: 'center',color: 'red' }}>Somethig went wrong...</h4>;
+    if(!this.state.error){
+     posts = this.state.posts.map(item => {
+         return <Post Clicked={() => this.postClick(item.id)} title={item.title} author={item.author}/>
+     })
+    }
+    return (
+      <div>
       <Header />
       <TopPost />
       <div className="container">
         <div className="row">
-        
-           <Post img={PostImg}/>
-           <Post img={PostImg3}/>
-           <Post img={PostImg2}/>
-           <Post img={PostImg}/>
-           <Post img={PostImg2}/>
-           <Post img={PostImg}/>
-           <Post img={PostImg3}/>
-
-           <Post img={PostImg3}/>
-           <Post img={PostImg2}/>
+            {posts}
         </div>
       </div>
     </div>
-  );
-};
+    )
+}
+}
 
 export default Homepage;
